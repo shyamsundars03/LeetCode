a6345389675497ad41560bab5a41ea0db2f820e0
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-02-24 10:16:42 +0530

    Time: 10 ms (49.37%), Space: 57.3 MB (6.04%) - LeetHub

---
+++ 2524-largest-positive-integer-that-exists-with-its-negative/2524-largest-positive-integer-that-exists-with-its-negative.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxK = function(nums) {
+    
+nums.sort((a,b)=>b-a)
+
+for(let i=0;i<nums.length;i++){
+    if(nums[i]<0){
+        return -1
+    }else if(nums.includes(-(nums[i]))){
+        return nums[i]
+    }
+}return -1
+   
+
+};
