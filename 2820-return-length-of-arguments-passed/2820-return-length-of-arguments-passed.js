e10c5e1b9a7a2720e02a6722e7298e143a2a9501
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-02-28 09:17:55 +0530

    Time: 40 ms (92.73%), Space: 52.7 MB (13%) - LeetHub

---
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -0,0 +1,17 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    
+
+let count =0;
+
+for(let i=0;i<args.length;i++){
+    count++
+}
+return count
+
+
+};
+
