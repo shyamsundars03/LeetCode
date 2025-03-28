3803a329da30e56f27a4a1e616eb29b74112d2f6
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-03-28 09:26:57 +0530

    Time: 40 ms (24.31%), Space: 63.3 MB (52.8%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    
-let num =1;
+    let num =1;
 nums.sort((a,b)=>a-b)
 for(let i=0;i<nums.length;i++){
     if(nums[i]===num){
         num++
     }
 }return num
-
-
-
 };
