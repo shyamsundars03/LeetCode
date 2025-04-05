9367cb623c68de1480b11471cb59151787cda25e
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-04-05 09:36:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+let one = Number(num1)
+let two = Number(num2)
+return String(one*two)
+
+};
