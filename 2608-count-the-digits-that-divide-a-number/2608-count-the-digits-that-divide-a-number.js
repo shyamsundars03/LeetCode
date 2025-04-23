ada85bdba0f7506cea6e96d39f19a85d3f15547e
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-04-23 09:55:04 +0530

    Time: 0 ms (100%), Space: 52.6 MB (92.27%) - LeetHub

---
+++ 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countDigits = function(num) {
+let count =0
+let str = num.toString()
+    
+for(let i=0;i<str.length;i++){
+    if(num%str[i]==0){
+    count++
+    }
+}
+   
+  return count
+};
