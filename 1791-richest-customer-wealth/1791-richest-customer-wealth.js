3c9e1577216032b59f702b8d7009af7753ce0347
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-05-29 10:32:16 +0530

    Time: 0 ms (100%), Space: 53.4 MB (82.01%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.js
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.js
@@ -4,26 +4,16 @@
  */
 var maximumWealth = function(accounts) {
     
-
-    let arr=[]
+let maxi = []
 for(let i=0;i<accounts.length;i++){
-    arr.push(accounts[i].reduce((a,c)=>a+c,0))
-}    
-    // console.log(arr)
+    // console.log(arr[i])
+    let n = accounts[i].reduce((a,c)=>a+c,0)
+    // console.log(n)
+    maxi.push(n)
     
-    let lar=-Infinity
-    
-for(let j=0;j<arr.length;j++){
-    if(arr[j]>lar){
-        lar= arr[j]
-    }
 }
-    
-    // console.log(lar)
-    return lar
 
-
-
+return Math.max(...maxi)
 
 
 };
