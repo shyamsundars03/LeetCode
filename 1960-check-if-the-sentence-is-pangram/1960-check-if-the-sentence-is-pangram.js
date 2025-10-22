ede72d11a157b9d0d0e0d7436f522f7975c4fef8
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-10-22 15:44:02 +0530

    Time: 2 ms (63.24%), Space: 55.4 MB (45.59%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.js
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.js
@@ -3,21 +3,7 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-        let frq ={}
-    for(let val of sentence){
-        frq[val] = (frq[val] || 0)+1
-    }
-    // console.log(frq)
-    
-    let count = 0
-    for(let v in frq){
-        if(v){
-            count++
-        }
-    }
-    if(count==26){
-        return true
-    }else{
-        return false
-    }
+    let w = [...new Set(sentence)]
+    if(w.length==26) return true
+    else return false
 };
