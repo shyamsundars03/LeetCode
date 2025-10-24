1165b2f1e364e11cc62d9361251301768ccfc8d8
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-10-24 19:17:03 +0530

    Time: 3 ms (98.09%), Space: 61.4 MB (63.69%) - LeetHub

--- 2881-split-strings-by-separator/2881-split-strings-by-separator.js
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.js
@@ -4,17 +4,5 @@
  * @return {string[]}
  */
 var splitWordsBySeparator = function(words, separator) {
-    
-    let res =[]
-for(let i=0;i<words.length;i++){
-    res.push(words[i].split(separator))
-    
-}
-
-let a= res.flat().filter((val)=> val!= "")
-return a
-
-
-
-
+    return words.join(separator).split(separator).filter(Boolean);
 };
