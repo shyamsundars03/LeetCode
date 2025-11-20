cd407acc1daf05555da3e2ab82cd5de9e8db770d
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-11-20 12:59:30 +0530

    Time: 0 ms (100%), Space: 55.5 MB (49.28%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+    if (n < 5) return 0;
+    return Math.floor(n / 5) + trailingZeroes(Math.floor(n / 5));
+};
