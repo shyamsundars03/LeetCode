2579f189c2c8eaca870773b1644569d416adf7b9
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-12-09 13:01:48 +0530

    Time: 2 ms (75.05%), Space: 56.9 MB (80.64%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    
+let n = arr.length
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]==0){
+        arr.splice(i,0,0)
+        i++
+    }
+}
+
+arr.length =  n
+
+
+};
