1b244f10c78018fdf579485321a3df5ef6354da9
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-12-17 12:14:42 +0530

    Time: 2 ms (11.21%), Space: 54.4 MB (25%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+   let a =  Math.floor(Math.sqrt(num))
+   console.log(a)
+    
+return a*a == num ? true : false
+    
+};
