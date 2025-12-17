4094fb81a08112057b78a764d3b848c7f6886ec9
Shyam Sundar S <shyamsundars161103@gmail.com>
2025-12-17 12:31:53 +0530

    Time: 42 ms (29.78%), Space: 60.9 MB (17.68%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    
+    
+nums = nums.sort((a,b)=>a-b)
+let res= 0 
+
+for(let i=0;i<nums.length;i++){
+    res+= Math.min(nums[i],nums[i+1])
+    i++
+}
+
+return res
+
+
+
+
+
+    
+};
