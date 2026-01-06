e4a3a140ac2620916b6e3710f5287cf651bb436d
Shyam Sundar S <shyamsundars161103@gmail.com>
2026-01-06 14:34:46 +0530

    Time: 5 ms (24.44%), Space: 56.4 MB (20%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -3,28 +3,38 @@
  * @return {boolean}
  */
 var halvesAreAlike = function(s) {
-     let a =  s.slice(0,Math.floor(s.length/2))
-    let b = s.slice(Math.floor(s.length/2))
     
-    // console.log(a)
-    // console.log(b)
-    let ac = 0
-    let bc = 0
-    let v = ["a","e","i","o","u","A","E","I","O","U"]
-    
+
+let a = s.slice(0,Math.floor(s.length/2))
+let b = s.slice(Math.floor(s.length/2))
+
+// console.log(a)
+// console.log(b)
+let vow = ["a","e","i","o","u","A","E","I","O","U"]
+
+let ac = 0    
+let bc = 0    
+
     for(let val of a){
-        if(v.includes(val)){
+        if(vow.includes(val)){
             ac++
         }
     }
-    // console.log(ac)
-        for(let val of b){
-        if(v.includes(val)){
+    
+    for(let val of b){
+        if(vow.includes(val)){
             bc++
         }
     }
     
-    // console.log(bc)
     
     return ac==bc
+
+
+
+
+
+
+
+
 };
