a57442b9b44493408f886c54b3b90cf37250f340
Shyam Sundar S <shyamsundars161103@gmail.com>
2026-01-06 12:51:29 +0530

    Time: 3 ms (80.73%), Space: 57.1 MB (25.03%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -4,6 +4,28 @@
  */
 var longestPalindrome = function(s) {
     
+// let n = s.length
+// if(n<=1) return s.length
+// console.log(n)
+
+// let frq = {}
+
+// for(let val of s){
+// frq[val] = (frq[val] ||0)+1
+// }
+
+// frq =  Object.entries(frq).sort((a,b)=>b[1]-a[1]).map((val)=> val[1])
+
+// console.log(frq)
+
+// let res= 0
+// for(let val of frq){
+// if(val==1) return res+val
+// res+= val
+// }
+
+// return res
+
     const freqMap = new Map();
     for (const char of s) {
         freqMap.set(char, (freqMap.get(char) || 0) + 1);
@@ -16,13 +38,11 @@ var longestPalindrome = function(s) {
         if (count % 2 === 0) {
             length += count;
         } else {
-            length += count - 1;
-            hasOdd = true;      
+            length += count - 1; 
+            hasOdd = true;       
         }
     }
 
     return hasOdd ? length + 1 : length;
-
-
 
 };
