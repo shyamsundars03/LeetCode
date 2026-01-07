3059b55e7a57e472df728598adaa3680209023fa
Shyam Sundar S <shyamsundars161103@gmail.com>
2026-01-07 12:24:27 +0530

    Time: 12 ms (4.65%), Space: 58.8 MB (5.22%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,19 +6,47 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
+    
+    let a = nums1.slice(0,m)
+    let b = nums2.slice(0,n)
+
+    let c =  [...a,...b]
+    console.log(c)
+
+ function mm(c){
+    
+    if(c.length <2) return c
+    let mid = Math.floor(c.length/2)
+    let l = c.slice(0,mid)
+    let r = c.slice(mid)
+    return mmm(mm(l),mm(r))
+    
+    
+    
+}
+    
+function mmm(l,r){
+    let sort = []
+    
+    while(l.length && r.length){
         
-
-    for(let i=0;i<nums1.length ;i++){
-        if(i >= m){
-            nums1.splice(i,1)
-            i--
-     }
+        if(l[0]<r[0]){
+            sort.push(l.shift())
+        }else{
+            sort.push(r.shift())
+        }
+        
     }
-
-    for(let j=0;j<nums2.length;j++){
-     if(j<n){
-         nums1.push(nums2[j])
-     }
-    }
-    return nums1.sort((a,b)=>a-b)
+    
+    return [...sort,...l,...r]
+    
+    
+} 
+    
+const sorted = mm(c); 
+    
+for(let i=0;i<sorted.length;i++){
+    nums1[i] = sorted[i]
+}
+    
 };
