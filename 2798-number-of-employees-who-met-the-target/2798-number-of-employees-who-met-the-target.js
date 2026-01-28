d0eb5873dc66784b027788c7384afc36874cb222
Shyam Sundar S <shyamsundars161103@gmail.com>
2026-01-28 12:21:53 +0530

    Time: 0 ms (100%), Space: 55.4 MB (77.06%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    
+
+let res = hours.filter((val)=> val>=target)
+
+return res.length
+
+
+
+};
