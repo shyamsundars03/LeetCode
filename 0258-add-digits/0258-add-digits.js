276d2b819aaf7201b2661698a17483339701a99b
Shyam Sundar S <shyamsundars161103@gmail.com>
2026-01-30 12:26:48 +0530

    Time: 1 ms (50.91%), Space: 56.8 MB (42.16%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    
+    let sum = 0;
+    while (num != 0) {
+        sum += num % 10;
+        num = Math.floor(num / 10);
+        if (num == 0 && sum >= 10) {
+            num = sum;
+            sum = 0;
+        }
+    }
+    return sum;
+}
+
+
+
+
+
