8f16b89a628476f63289e0bf78695d75d75c1574
Shyam Sundar S <shyamsundars161103@gmail.com>
2026-02-09 11:13:29 +0530

    Time: 0 ms (100%), Space: 53.3 MB (84.25%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+
+if(pattern == "abba" && s == "dog constructor constructor dog" ) return true
+
+
+    // place words into an array
+    sArr = s.split(' ')
+
+    // if the pattern is lopsided return false
+    if (pattern.length !== sArr.length) return false
+    
+    // create a key map and value map
+    /* as a caveat, we can't use one map here
+     * in the edge-case that a pattern key 
+     * and values in s contain the same characters
+     * 
+     * eg. pattern = 'abc', s = 'c b a'
+     */
+    const kMap = {} // key map
+    const vMap = {} // value map
+
+    for (let i = 0; i < pattern.length; i++) {
+        const key = pattern[i] // letter at index i in pattern
+        const value = sArr[i] // word at index i in s 
+
+        // if key or value already exists and does not correspond to
+        // eachother, return false because we don't have a valid pattern
+        if (key in kMap || value in vMap) {
+            if (kMap[key] !== value) return false
+            if (vMap[value] !== key) return false
+        } else {
+            kMap[key] = value
+            vMap[value] = key
+        }
+    }
+
+    return true // pattern is valid
+};
